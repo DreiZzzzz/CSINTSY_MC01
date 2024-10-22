c4acdc1f8ef98b5399d26142706ab85ccb2844fa
Andrei Zarmin De Jesus <161214656+DreiZzzzz@users.noreply.github.com>
2024-10-22 10:36:52 +0800

    added to checker

--- CSINTSY/src/Checker.java
+++ CSINTSY/src/Checker.java
@@ -11,6 +11,7 @@ public class Checker {
         Scanner sc = new Scanner(System.in);
         String fileName;
         int userChoice;
+        int selectAlgo;
         boolean reLoop = false;
 
         System.out.println("Current working directory: " + System.getProperty("user.dir"));
@@ -29,9 +30,113 @@ public class Checker {
             }
         } while(recordHolder == null);  // re-loop until the file is read correctly
 
-        System.out.println("Neighbours of B: " + recordHolder.getActualCost().get(16).size()); // 4
-        // If file is read successfully, print connections
-        recordHolder.printConnections();
+       // System.out.println("Neighbours of B: " + recordHolder.getActualCost().get(16).size()); // 4
+        do {
+            userChoice = getUserChoice(sc);
+            switch (userChoice) {
+                case 1: recordHolder.printHeuristicValue();
+                    break;
+                case 2: recordHolder.printConnections();
+                    break;
+                case 3:
+                    displayNodes();
+                  //   selectAlgo = getEndStart(sc);
+                    switch (selectAlgo) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                    }
+
+                    break;
+                case 4: System.out.println("!! PROGRAM EXIT SUCCESSFUL !!");
+                    break;
+            }
+
+
+        } while (userChoice != 4);
     }
+
+    public static int getUserChoice(Scanner sc) {
+        int userChoice = 0;
+
+        System.out.println("!! USER OPTIONS !!");
+        System.out.println("(1) VIEW NODES AND HEURISTIC VALUE");
+        System.out.println("(2) VIEW CONNECTIONS AND COST OF PATH");
+        System.out.println("(3) TEST ALGORITHM (UCS/A*)");
+        System.out.println("(4) EXIT PROGRAM");
+
+        System.out.print("Enter choice [1-3]: ");
+        userChoice = sc.nextInt();
+        System.out.println(); // newline
+
+        return userChoice;
+    }
+
+    public static void displayNodes() {
+        int ctr = 0;
+
+        String[] nodeName = { "A", "B", "C",
+                              "D", "E", "F",
+                              "G", "H", "I",
+                              "J1", "J2", "K",
+                              "L", "M", "N",
+                              "O", "P", "Q",
+                              "R", "S", "T",
+                              "U"};
+        String[] fullName= { "University Mall", "McDonald's", "Perico's", // A-C
+                     "Bloemen Hall", "W.H Taft Residence", "EGI TAFT", // D-F
+                     "Castro Street", "Agno Food Court", "One Archers'", // G-I
+                     "Andrew's La Casita", "Enrique's La Casita", "Green Mall", //J1-K
+                     "Green Court", "Sherwood", "Jollibee", //L-N
+                     "Dagonoy St.", "Burgundy", "Estrada St.", // O-Q
+                     "D' Student's Place", "Leon Guinto St.", "P.Ocampo St.", // R-T
+                     "Fidel A. Reyes St."}; // U
+
+        System.out.println("!! ALL NODES !!");
+        for (int i = 0; i < nodeName.length; i++) {
+            System.out.print(fullName[i] + "(" + nodeName[i] + " = " + i + ")" + "\t\t");
+            ++ctr;
+
+            if (ctr == 2) {
+                ctr = 0;
+                System.out.println(); //newline
+            }
+        }
+        System.out.println(); // newline
+    }
+
+    // ask for the algo
+    public int getEndStart(Scanner sc) {
+        int startNode = 0;
+        int endNode = 0;
+        boolean isValid = false;
+        boolean isNotMatch = false;
+
+        while (!isValid) {
+            System.out.println("Enter start node(int): ");
+            startNode = sc.nextInt();
+
+            if (startNode >= 0 && startNode <= 21) {
+                isValid = true;
+            }
+        }
+
+        while (!isNotMatch) {
+            System.out.println("Enter end node(int): ");
+            endNode = sc.nextInt();
+
+            if (endNode >= 0 && endNode <= 21) {
+                if (endNode != startNode) {
+                    isNotMatch = true;
+                }
+            }
+
+        }
+
+
+
+    }
+
 }
 
--- CSINTSY/src/DataRecord.java
+++ CSINTSY/src/DataRecord.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
+
 public class DataRecord {
-    // Map<Node, hValue>
     private Map<Integer, Double> parentNodeHeuristicMap; // To store node and its heuristic value
     private Map<Integer, List<List<Integer>>> connectionsMap;  // To store node connections (parent -> [cost, neighbour])
-
     private int numNodes;
     private int numConnections;
     /**
      * The sole constructor of the Graph class
      *
      * @param numNodes an int that holds the total number of nodes
+     * @param numConnections an int that holds the total number of connections in the graph
      */
     public DataRecord(int numNodes, int numConnections) {
         this.numNodes = numNodes;
@@ -19,10 +19,23 @@ public class DataRecord {
         this.connectionsMap = new HashMap<>();
     }
 
+    /**
+     * Adds a node, and it's corresponding heuristic value in the map.
+     *
+     * @param parentNodeLabel
+     * @param heuristicValue
+     */
     public void addParentNode(int parentNodeLabel, double heuristicValue) {
         this.parentNodeHeuristicMap.put(parentNodeLabel, heuristicValue);
     }
 
+    /**
+     * Maps the cost of path and its neighbour to it's connections
+     *
+     * @param currNode an int that
+     * @param costOfPath an int that holds the cost of path between
+     * @param neighbourName holds the neighbourName in int form
+     */
     public void addNeighbours(int currNode, int costOfPath, int neighbourName) {
         // Create a new ArrayList if there is no existing list for currNode
         this.connectionsMap.putIfAbsent(currNode, new ArrayList<>());
@@ -37,10 +50,19 @@ public class DataRecord {
         this.connectionsMap.get(currNode).add(neighbourInfo);
     }
 
+    /**
+     *
+     * @return
+     */
     public Map<Integer, Double> getHeuristic() {
         return this.parentNodeHeuristicMap;
     }
 
+    /**
+     *
+     *
+     * @return
+     */
     public Map<Integer, List<List<Integer>>> getActualCost(){
         return this.connectionsMap;
     }
@@ -70,14 +92,33 @@ public class DataRecord {
             }
             System.out.println();
         }
+        System.out.println(); // newline
     }
 
+    /**
+     *
+     * @param index
+     * @return
+     */
     public int getTotalNeighbours(int index) {
         return connectionsMap.get(index).size();
     }
 
+    /**
+     *
+     */
     public void printHeuristicValue() {
+        String[] nodeName = {
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J1",
+                "J2", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                "T", "U"
+        };
 
+        for (Map.Entry<Integer, Double> entry : parentNodeHeuristicMap.entrySet()) {
+            Integer key = entry.getKey();
+            Double value = entry.getValue();
+            System.out.println("Key: (" + key + " = " + nodeName[key] + "), Value: " + value);
+        }
     }
 }
 
